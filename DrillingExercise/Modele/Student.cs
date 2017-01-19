d48a5ff94e844d842dc6b9b524832fc8372de0b9
neromule@gmail.com <deroot@info.iutamiens>
2017-01-19 16:50:48 +0100

    Travail sur les classes User et Student

---
+++ DrillingExercise/Modele/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modele
+{
+    class Student : User
+    {
+        private List<Theme> toDo;
+        private Dictionary<Theme, int> marks;
+        private Dictionary<Theme, TimeSpan> remainingTime;
+
+        public List<Theme> ToDo
+        {
+            get
+            {
+                return toDo;
+            }
+        }
+
+        public int getMark(Theme t)
+        {
+            return marks[t];
+        }
+
+        public Student(String firstName, String lastName, String mail, String password) : base(firstName, lastName, mail, password)
+        {
+
+        }
+    }
+}
